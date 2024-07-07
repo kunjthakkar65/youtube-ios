b350bdfbd3ca9fa3187db863dd1ca2635c6193ae
tkniranjanbhai <tkniranjanbhai@tekion.com>
2024-07-07 23:01:37 +0530

    initial xcode project creation

---
+++ youtube-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-ios
+//
+//  Created by Thakkar Kunj Niranjanbhai on 07/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
